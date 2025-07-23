7d683790b541477d353b27e5bf744cdb3636f530
rocklain <ztgk.3142@gmail.com>
2025-07-23 23:24:13 +0900

    フォームによるユーザー登録処理を実装

--- app.py
+++ app.py
@@ -3,9 +3,74 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+from pytz import timezone
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "mysecretkey"
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    administrator = db.Column(db.String(1))
+    post = db.relationship('BlogPost', backref='author', lazy='dynamic')
+
+    def __init__(self, email, username, password_hash, administrator):
+        self.email = email
+        self.username = username
+        self.password_hash = password_hash
+        self.administrator = administrator
+
+    def __repr__(self):
+        return f"UserName: {self.username}"
+
+
+class BlogPost(db.Model):
+    __tablename__ = 'blog_post'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    date = db.Column(db.DateTime, default=datetime.now(timezone('Asia/Tokyo')))
+    title = db.Column(db.String(140))
+    text = db.Column(db.Text)
+    summary = db.Column(db.String(140))
+    featured_image = db.Column(db.String(140))
+
+    def __init__(self, title, text, featured_image, user_id, summary):
+        self.title = title
+        self.text = text
+        self.featured_image = featured_image
+        self.user_id = user_id
+        self.summary = summary
+
+    def __repr__(self):
+        return f"PostID: {self.id}, Title: {self.title}, Author: {self.author} \n"
 
 
 class RegistrationForm(FlaskForm):
@@ -22,9 +87,14 @@ class RegistrationForm(FlaskForm):
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        session["email"] = form.email.data
-        session["username"] = form.username.data
-        session["password"] = form.password.data
+        # session["email"] = form.email.data
+        # session["username"] = form.username.data
+        # session["password"] = form.password.data
+        user = User(email=form.email.data, username=form.username.data,
+                    password_hash=form.password.data, administrator="0")
+        db.session.add(user)
+        db.session.commit()
+        
         flash('ユーザーが登録されました')
         return redirect(url_for("user_maintenance"))
     return render_template("register.html", form=form)
